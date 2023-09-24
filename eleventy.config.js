0c087160516a6c6dcc20915b3f9aec39df921ba8
Taiki Kanda <taiki@konnyak.com>
2023-09-24 10:10:41 +0000

    add date filter and update post layout.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 const eleventyNavigation = require("@11ty/eleventy-navigation");
 const { EleventyI18nPlugin } = require("@11ty/eleventy");
 
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.ignores.add("README.md");
 
@@ -18,6 +20,14 @@ module.exports = function(eleventyConfig) {
 			}
 		}
 		return pageArray;
+	});
+
+	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "yyyy-LL-dd");
+	});
+
+	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
 	});
 
 	return {
